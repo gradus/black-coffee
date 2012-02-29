d67e3707c6fdb3a0e449a901cc71efab3256e531
gradus <kriswindham@gmail.com>
2012-02-28 21:02:50 -0500

    update test

--- app.js
+++ app.js
@@ -1,107 +1,104 @@
-(function() {
-  var app, coffeecup, flatiron;
+var app, coffeecup, flatiron;
 
-  coffeecup = require('coffeecup');
+coffeecup = require('coffeecup');
 
-  flatiron = require('flatiron');
+flatiron = require('flatiron');
 
-  app = flatiron.app;
+app = flatiron.app;
 
-  app.use(flatiron.plugins.http);
+app.use(flatiron.plugins.http);
 
-  app.router.get('/', function() {
-    this.res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    return this.res.end(coffeecup.render(function() {
-      doctype(5);
-      return html(function() {
-        head(function() {
-          title('I like my Coffee Black');
-          meta({
-            name: 'viewport',
-            content: 'width=device-width, initial-scale=0.7, maximum-scale=0.8'
-          });
-          script({
-            src: 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'
-          });
-          style('body {\n  font-family: sans-serif;\n  background-color:black; \n  color:whitesmoke;\n  margin: 30px 30px;\n  padding: 0px;\n  text-align: center;\n}\n#wrap {\n  width:500px;\n  margin:0px auto;\n  text-align:left;\n}\na, a:visited, a:hover { color:white;}\np { color: white }\nheader, nav, section, footer {display: block}');
-          return coffeescript(function() {
-            return $(document).ready(function() {
-              $('.black_coffee').hide();
-              $('.like_it_black').hide();
-              $('.text').hide();
-              $('.fade_img').hide();
-              $('.credits').hide();
-              $('.black_coffee').fadeIn(1000);
-              $('.like_it_black').fadeIn(3000);
-              $('.text').fadeIn(4000);
-              $('.fade_img').fadeIn(8000);
-              return $('.credits').fadeIn(10000);
-            });
+app.router.get('/', function() {
+  this.res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  return this.res.end(coffeecup.render(function() {
+    doctype(5);
+    return html(function() {
+      head(function() {
+        title('I like my Coffee Black');
+        meta({
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=0.7, maximum-scale=0.8'
+        });
+        script({
+          src: 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'
+        });
+        style('body {\n  font-family: sans-serif;\n  background-color:black; \n  color:whitesmoke;\n  margin: 30px 30px;\n  padding: 0px;\n  text-align: center;\n}\n#wrap {\n  width:500px;\n  margin:0px auto;\n  text-align:left;\n}\na, a:visited, a:hover { color:white;}\np { color: white }\nheader, nav, section, footer {display: block}');
+        return coffeescript(function() {
+          return $(document).ready(function() {
+            $('.black_coffee').hide();
+            $('.like_it_black').hide();
+            $('.text').hide();
+            $('.fade_img').hide();
+            $('.credits').hide();
+            $('.black_coffee').fadeIn(1000);
+            $('.like_it_black').fadeIn(3000);
+            $('.text').fadeIn(4000);
+            $('.fade_img').fadeIn(8000);
+            return $('.credits').fadeIn(10000);
           });
         });
-        return body(function() {
-          return div({
-            id: 'wrap'
+      });
+      return body(function() {
+        return div({
+          id: 'wrap'
+        }, function() {
+          h1({
+            "class": 'black_coffee'
           }, function() {
-            h1({
-              "class": 'black_coffee'
+            return 'Black Coffee';
+          });
+          h3({
+            "class": 'like_it_black'
+          }, function() {
+            return '"I like my coffee black"';
+          });
+          p({
+            "class": 'text'
+          }, function() {
+            text('Built w/ ');
+            a({
+              href: 'https://github.com/twilson63/iron-coffee'
             }, function() {
-              return 'Black Coffee';
+              return 'iron-coffee';
             });
-            h3({
-              "class": 'like_it_black'
+            text(', a ');
+            a({
+              href: 'http://coffeescript.org/'
             }, function() {
-              return '"I like my coffee black"';
+              return 'CoffeeScript';
             });
-            p({
-              "class": 'text'
+            br();
+            text(' and ');
+            a({
+              href: 'http://http://flatironjs.org/'
             }, function() {
-              text('Built w/ ');
-              a({
-                href: 'https://github.com/twilson63/iron-coffee'
-              }, function() {
-                return 'iron-coffee';
-              });
-              text(', a ');
-              a({
-                href: 'http://coffeescript.org/'
-              }, function() {
-                return 'CoffeeScript';
-              });
-              br();
-              text(' and ');
-              a({
-                href: 'http://http://flatironjs.org/'
-              }, function() {
-                return 'Flatiron';
-              });
-              return text(' http server template.');
+              return 'Flatiron';
             });
-            img({
-              "class": 'fade_img',
-              src: "http://static.flickr.com/44/143391932_886e771618.jpg"
-            });
-            return p({
-              "class": 'credits',
-              style: 'float:right;padding-right:230px;'
+            return text(' http server template.');
+          });
+          img({
+            "class": 'fade_img',
+            src: "http://static.flickr.com/44/143391932_886e771618.jpg"
+          });
+          return p({
+            "class": 'credits',
+            style: 'float:right;padding-right:230px;'
+          }, function() {
+            text('created by ');
+            return a({
+              href: 'http://github.com/gradus'
             }, function() {
-              text('created by ');
-              return a({
-                href: 'http://github.com/gradus'
-              }, function() {
-                return 'Kris Windham';
-              });
+              return 'Kris Windham';
             });
           });
         });
       });
-    }));
-  });
+    });
+  }));
+});
 
-  app.start(3000);
+app.start(3000);
 
-  console.log('listening on port 3000');
-
-}).call(this);
+console.log('listening on port 3000');
--- test/app.coffee
+++ test/app.coffee
@@ -4,5 +4,7 @@ request = require 'request'
 describe 'app', ->
   it 'should respond to root', (done) ->
     request.get 'http://localhost:3000/', (e, b, r) ->
-      console.log b
+      if b == undefined
+        console.log 'fire up your app with node server.js for this test to pass' 
+      b.should != undefined
       done()
